b5455849d1051dcdce5ef60bea82e947b5a97ad7
SebastianEgg <bastian.egger@gmx.at>
2022-01-27 08:08:26 +0100

    Projektbericht 27.1.2022 und kleine Aenderungen am Backend zum testen von Daten

--- Backend/iot.net/Iot.Net/SnQPoolIot/SnQPoolIot.ConApp/Program.cs
+++ Backend/iot.net/Iot.Net/SnQPoolIot/SnQPoolIot.ConApp/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using MQTTnet;
+﻿using MQTTnet;
 using MQTTnet.Client.Connecting;
 using MQTTnet.Client.Disconnecting;
 using MQTTnet.Client.Options;
@@ -25,8 +23,44 @@ namespace SnQPoolIot.ConApp
         static partial void ClassConstructed();
         #endregion Class-Constructors
 
-        private static void Main(/*string[] args*/)
+        private static async Task Main(/*string[] args*/)
         {
+
+
+
+            Console.WriteLine("SnQPoolIot");
+            var mqttClientId = "PooliotClient";             // Unique ClientId or pass a GUID as string for something random
+            var mqttBrokerAddress = "192.168.178.44";         // hostname or IP address of your MQTT broker
+            var mqttBrokerUsername = "pi";       // Broker Auth username if using auth
+            var mqttBrokerPassword = "egg21Pool";       // Broker Auth password if using auth
+            var topic = "pooliot/noise/state";                 // topic to subscribe to
+
+            var mqttClient = new MqttFactory().CreateManagedMqttClient();
+            var mqttClientOptions = new ManagedMqttClientOptionsBuilder()
+                        .WithAutoReconnectDelay(TimeSpan.FromSeconds(2))
+                        .WithClientOptions(new MqttClientOptionsBuilder()
+                            .WithTcpServer(mqttBrokerAddress, 1883)
+                            .WithClientId(mqttClientId)
+                            .WithCredentials(mqttBrokerUsername, mqttBrokerPassword)     // Remove this line if no auth
+                            .WithCleanSession()
+                            .Build()
+                        )
+                        .Build();
+
+            Console.WriteLine(mqttClient);
+            mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(e => MqttOnNewMessage(e));
+            mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(e => MqttOnConnected(e));
+            mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(e => MqttOnDisconnected(e));
+
+            await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).WithExactlyOnceQoS().Build());
+            await mqttClient.StartAsync(mqttClientOptions);
+            Console.ReadLine();
+            Console.ReadLine();
+
+
+
+
+
 
             Console.WriteLine(DateTime.Now);
 
@@ -41,8 +75,8 @@ namespace SnQPoolIot.ConApp
         {
             // Do something with each incoming message from the topic
             var eggerAgain = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-                Console.WriteLine($"MQTT Client: OnNewMessage Topic: {e.ApplicationMessage.Topic} / Message: {eggerAgain}");
-             
+            Console.WriteLine($"MQTT Client: OnNewMessage Topic: {e.ApplicationMessage.Topic} / Message: {eggerAgain}");
+
         }
 
         private static void MqttOnConnected(MqttClientConnectedEventArgs e)
