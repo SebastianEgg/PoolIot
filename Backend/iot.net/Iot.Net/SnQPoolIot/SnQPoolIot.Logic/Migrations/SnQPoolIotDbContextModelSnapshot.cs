5389d6a7cd917eb61e7204d7e3c1fbba51026074
SebastianEgg <bastian.egger@gmx.at>
2022-02-20 18:02:51 +0100

    Added Latex Files

--- Backend/iot.net/Iot.Net/SnQPoolIot/SnQPoolIot.Logic/Migrations/SnQPoolIotDbContextModelSnapshot.cs
+++ Backend/iot.net/Iot.Net/SnQPoolIot/SnQPoolIot.Logic/Migrations/SnQPoolIotDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace SnQPoolIot.Logic.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.10")
+                .HasAnnotation("ProductVersion", "5.0.13")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("SnQPoolIot.Logic.Entities.Persistence.Account.Access", b =>
